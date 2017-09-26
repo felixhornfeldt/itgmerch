46e140e05e86b5ac3b4967bf3ed109690c64023c
Felix Hörnfeldt <felix.hornfeldt@learnet.se>
2017-09-26 15:50:05 +0200

    Take away color from about.html front-back'

--- ITGMarketplace/JS/main.js
+++ ITGMarketplace/JS/main.js
@@ -1,26 +1,18 @@
-function calculate() {
-    var myAntal = document.getElementById ("antal").value;
-    var result = document.getElementById ("result");
-    var myResult = 80 * myAntal;
-    result.value = myResult  + "kr.";
-}
-
-function calculate1() {
-    var myAntal = document.getElementById ("antal1").value;
-    var result = document.getElementById ("result1");
-    var myResult = 180 * myAntal;
-    result.value = myResult + "kr.";
-}
-
-// For hesret.html
-
-/*function calculate() {
-    var price = document.getElementById ("price");
-    var myAntal = document.getElementById ("antal").value;
-    var result = document.getElementById ("result");
-    var myResult = price * myAntal;
-    result.value = myResult  + "kr.";
-}*/
-
-
-
+simpleCart({
+    checkout: { 
+        type: "SendForm" , 
+        url: "http://example.com/your/custom/checkout/url" ,
+        // http method for form, "POST" or "GET", default is "POST"
+        method: "POST" , 
+        // url to return to on successful checkout, default is null
+        success: "success.html" , 
+        // url to return to on cancelled checkout, default is null
+        cancel: "cancel.html" ,
+        // an option list of extra name/value pairs that can
+        // be sent along with the checkout data
+        extra_data: {
+          storename: "Bob's cool plumbing store",
+          cartid: "12321321"
+        }
+    } 
+});
