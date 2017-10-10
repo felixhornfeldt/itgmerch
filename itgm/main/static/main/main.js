b865f128b9b5f3136c916244f84eba6be375c29c
Emil Babayev <emil.babayev@learnet.se>
2017-10-10 11:39:30 +0200

    Integrate into server

--- itgm/main/static/main/main.js
+++ itgm/main/static/main/main.js
@@ -1,13 +1,26 @@
 simpleCart({
     checkout: { 
         type: "SendForm" , 
-        url: "/cart/" ,
+        url: "/order/" ,
         // http method for form, "POST" or "GET", default is "POST"
         method: "POST" , 
-        
+        // url to return to on successful checkout, default is null
+        success: "success.html" , 
+        // url to return to on cancelled checkout, default is null
+        cancel: "cancel.html" ,
+        // an option list of extra name/value pairs that can
+        // be sent along with the checkout data
         extra_data: {
-          storename: "Bob's cool plumbing store",
-          cartid: "12321321"
+          order: document.getElementsByClassName(simpleCart_items)
         }
     } 
 });
+
+
+function showDiv() {
+    document.getElementById('show').style.display = "block";
+}
+
+function removeDiv() {
+    document.getElementById('show').style.display = "none";
+}
