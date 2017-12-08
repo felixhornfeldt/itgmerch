4e8e233e0226bd4a7de69f3ec80c7066bdb462f9
Felix Hörnfeldt <felix.hornfeldt@learnet.se>
2017-12-08 08:43:32 +0100

    Major and important change to simplecartjs script! Important to Update EMIL!!!

--- ITGMarketplace/wojodesign-simplecart-js-51a9a38/simpleCart.js
+++ ITGMarketplace/wojodesign-simplecart-js-51a9a38/simpleCart.js
@@ -82,13 +82,14 @@
 					"SEK": { code: "SEK", symbol: "SEK&nbsp;", name: "Swedish Krona" },
 					"CHF": { code: "CHF", symbol: "CHF&nbsp;", name: "Swiss Franc" },
 					"THB": { code: "THB", symbol: "&#3647;", name: "Thai Baht" },
-					"BTC": { code: "BTC", symbol: " BTC", name: "Bitcoin", accuracy: 4, after: true	}
+					"BTC": { code: "BTC", symbol: " BTC", name: "Bitcoin", accuracy: 4, after: true	},
+					"kr": { code: "kr", symbol: "&nbsp;kr", name: "Swedish Krona", after: true }
 				},
 
 				// default options
 				settings = {
 					checkout				: { type: "PayPal", email: "you@yours.com" },
-					currency				: "SEK",
+					currency				: "kr",
 					language				: "english-us",
 
 					cartStyle				: "table",
