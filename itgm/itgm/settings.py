6da8eca74140da89b98618f2be0edb129472bff8
Emil Babayev <zentabit@gmail.com>
2018-02-09 11:46:27 +0100

    Update email info

--- itgm/itgm/settings.py
+++ itgm/itgm/settings.py
@@ -131,12 +131,12 @@ STATIC_ROOT = BASE_DIR + '/static/'
 
 # Email settings
 
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
+EMAIL_HOST = "mail.itgmarket.se"
+EMAIL_PORT = 25
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = "test.itgmarket@gmail.com"
-EMAIL_HOST_PASSWORD = "bmBXTamwuVsZZqH3SRyc"
+EMAIL_HOST_USER = "support@itgmarket.se"
+EMAIL_HOST_PASSWORD = "justtothrowitaway"
 
 # Social auth stuff
 
